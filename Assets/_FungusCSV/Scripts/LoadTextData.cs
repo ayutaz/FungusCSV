c6c0a405fe0990d3e2fd14711ff908b6d55e7797
yousan <ka1357amnbpdr@gmail.com>
2022-08-01 20:36:28 +0900

    fix: GSheetからデータの取得、Fungusフォーマットへの変換完了

---
+++ Assets/_FungusCSV/Scripts/LoadTextData.cs
@@ -0,0 +1,47 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace _FungusCSV
+{
+    public class LoadTextData
+    {
+        private const string url = "https://script.google.com/macros/s/AKfycbzyK8eY1JpBGWrNM8TAa731Op0jFwqI1x0xsuBc9BZgb9qpz7I0PHf2nHdJTHt3hqCk-Q/exec";
+        private const string sheetName = "info";
+
+        /// <summary>
+        /// ゲーム情報をスプレッドシートから取得
+        /// </summary>
+        /// <returns></returns>
+        public static async UniTask<T> GetGameInfo<T>()
+        {
+            var request = UnityWebRequest.Get($"{url}?sheetName={sheetName}");
+            await request.SendWebRequest();
+            if (request.result is UnityWebRequest.Result.ConnectionError or UnityWebRequest.Result.ProtocolError or UnityWebRequest.Result.DataProcessingError)
+            {
+                Debug.Log("fail to get card info from google sheet");
+            }
+            else
+            {
+                var json = request.downloadHandler.text;
+                var data = JsonUtility.FromJson<T>(json);
+                return data;
+            }
+
+            return default;
+        }
+
+        public static string ConvertFungusTextFormat(TextDataList data)
+        {
+            var fungusText = "";
+            foreach (var info in data.textData)
+            {
+                fungusText += $"#{info.Key}\n";
+                fungusText += $"{info.Standard}\n";
+                fungusText += "\n";
+            }
+
+            return fungusText;
+        }
+    }
+}
---
+++ Assets/_FungusCSV/Scripts/TextData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _FungusCSV
+{
+    [Serializable]
+    public class TextData
+    {
+        public string Key;
+        public string Description;
+        public string Standard;
+    }
+
+    [Serializable]
+    public class TextDataList
+    {
+        public List<TextData> textData;
+    }
+}
