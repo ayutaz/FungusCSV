5ff304844277ff0957db1f1e36af62eff36373d8
yousan <ka1357amnbpdr@gmail.com>
2022-08-02 00:26:43 +0900

    feat: GSheetからのデータのロードまでFlowchartの自動実装を一時停止

--- Assets/_FungusCSV/Scripts/UpdateFungusText.cs
+++ Assets/_FungusCSV/Scripts/UpdateFungusText.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using Fungus;
+﻿using Fungus;
 using UnityEngine;
 
 namespace _FungusCSV
 {
     public class UpdateFungusText : MonoBehaviour
     {
-        [SerializeField] private TextDataList textDataList;
-        [SerializeField] private Flowchart flowchart;
         private Localization _localization;
+        [SerializeField] private GameStarted gameStarted;
 
         private void Awake()
         {
@@ -18,11 +16,9 @@ namespace _FungusCSV
         private async void Start()
         {
             var data = await LoadTextData.GetGameInfo<TextDataList>();
-            textDataList = data;
             var fungusText = LoadTextData.ConvertFungusTextFormat(data);
-            Debug.Log(fungusText);
             _localization.SetStandardText(fungusText);
-            flowchart.enabled = true;
+            gameStarted.enabled = true;
         }
     }
 }
