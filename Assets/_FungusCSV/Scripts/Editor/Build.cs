47d19e33c1c1d201f43558b2a7e7852f6f29cef4
yousan <ka1357amnbpdr@gmail.com>
2022-08-05 18:20:59 +0900

    feat: buildの後処理(ビルドデータを特定のブランチのpush)の追加

---
+++ Assets/_FungusCSV/Scripts/Editor/Build.cs
@@ -0,0 +1,40 @@
+﻿#if UNITY_EDITOR
+using System.Diagnostics;
+using System.IO;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+namespace _FungusCSV
+{
+    public class Build : IPostprocessBuildWithReport
+    {
+        public int callbackOrder { get; }
+        private string _batPath;
+
+        public void OnPostprocessBuild(BuildReport report)
+        {
+            _batPath = Path.Combine(GetBuildFolderPath(), "Build");
+            RunCommand(_batPath);
+        }
+
+        private static string GetBuildFolderPath()
+        {
+            return Directory.GetParent(Application.dataPath)?.FullName;
+        }
+
+        private static void RunCommand(string buildFolderPath)
+        {
+            var process = new Process();
+            process.StartInfo.WorkingDirectory = buildFolderPath;
+            process.StartInfo.FileName = Path.Combine(buildFolderPath, "Deploy.bat");
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardInput = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.CreateNoWindow = true;
+            process.Start();
+            process.WaitForExit();
+        }
+    }
+}
+#endif
